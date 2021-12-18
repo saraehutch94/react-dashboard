fa6d34754267333366188f4bac835edc58ee7276
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-18 17:59:15 -0500

    reorganize css code for global and component-specific styles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,15 @@
 function Sidebar() {
   return (
-    <nav>
-      <div>Dashboard</div>
-      <div>Widget</div>
-      <div>Reviews</div>
-      <div>Customers</div>
-      <div>Online Analysis</div>
-      <div>Settings</div>
-    </nav>
+    <div className="nav-section">
+      <nav>
+        <div>Dashboard</div>
+        <div>Widget</div>
+        <div>Reviews</div>
+        <div>Customers</div>
+        <div>Online Analysis</div>
+        <div>Settings</div>
+      </nav>
+    </div>
   );
 }
 
