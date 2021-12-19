071faf9349eae1b0f5dd0ac35f766afa474cbe39
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-19 16:45:56 -0500

    add icons to sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -11,19 +11,24 @@ function Sidebar() {
           />
           <div className="nav-item-dashboard">Dashboard</div>
         </div>
-        <div>
+        <div className="nav-item-wrapper">
+          <img className="inline-img" src="https://i.imgur.com/kAFud9I.png" />
           <div className="nav-item">Widget</div>
         </div>
-        <div>
+        <div className="nav-item-wrapper">
+          <img className="inline-img" src="https://i.imgur.com/4NqbVDD.png" />
           <div className="nav-item">Reviews</div>
         </div>
-        <div>
+        <div className="nav-item-wrapper">
+          <img className="inline-img" src="https://i.imgur.com/m2h4a9G.png" />
           <div className="nav-item">Customers</div>
         </div>
-        <div>
+        <div className="nav-item-wrapper">
+          <img className="inline-img" src="https://i.imgur.com/ePFhQRk.png" />
           <div className="nav-item">Online Analysis</div>
         </div>
-        <div>
+        <div className="nav-item-wrapper">
+          <img className="inline-img" src="https://i.imgur.com/PRy9cyc.png" />
           <div className="nav-item">Settings</div>
         </div>
       </nav>
